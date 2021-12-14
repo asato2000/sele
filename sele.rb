8dfac354779d8451adc6d4b638590e48eb1972ff
asato2000 <68488182+asato2000@users.noreply.github.com>
2021-12-14 15:16:04 +0900

    サイト名取得

--- sele.rb
+++ sele.rb
@@ -8,21 +8,24 @@ session.manage.timeouts.implicit_wait = 10
 session.navigate.to "https://google.com/"
 
 # ページのタイトルを出力する
-puts session.title
+# puts session.title
 
 # 検索フォームの取得(この場合はname属性で取得している)
 query = session.find_element(:name, 'q')
 # "zenn"を自動入力する
-query.send_keys('wen制作会社　パートナー')
+query.send_keys('web制作会社　パートナー')
 
 # # 送信(検索)
 query.submit
+
+puts session.find_element(:xpath,"/html/body/div[7]/div/div[10]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div[1]/a/h3
+  ").text
 
 # # 5秒遅延(処理が早すぎてページ遷移前にスクリーンショットされてしまうため)
 sleep(5)
 
 # # スクリーンショットをして"zenn.png"で保存する(保存される場所は、コード実行箇所)
-# if session.save_screenshot('zenn.png')
+# if session.save_screenshotzenn.png')
 #   # スクリーンショットができたら出力する
 #   puts "スクリーンショットされました！"
 # end
